71ff67cad643f154df147231c6ac564b9ea92d57
tigerfirewhite203203-cpu <tigerfirewhite.203203@gmail.com>
2026-04-11 10:40:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// On attend que le bouton soit cliqué
-document.getElementById('btnAction').addEventListener('click', function() {
-    // On affiche le message caché
-    const message = document.getElementById('message');
-    message.classList.remove('hidden');
-    
-    // Petit effet de console pour vérifier
-    console.log("Le bouton a été cliqué !");
-});
